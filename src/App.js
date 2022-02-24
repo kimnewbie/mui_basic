6a58443f3d495dd648586d304db33751f2b3a0db
youjininreallife <gracecancho@gmail.com>
2022-02-24 13:17:02 +0900

    deleted style etc

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
+import SignInOutContainer from "./containers";
 import './App.css';
-import { makeStyles } from '@mui/styles';
-import SideMenu from "./codAffection/SideMenu";
-import Header from './codAffection/Header';
-import React from 'react';
-import { CssBaseline } from '@mui/material';
-
-const useStyles = makeStyles({
-  appMain: {
-    paddingLeft: '320px',
-    width: '100%'
-  }
-})
 
 const App = () => {
-  const classes = useStyles();
-
   return (
     <div className="App">
-      <SideMenu />
-      <div className={classes.appMain}>
-        <Header />
-      </div>
-      <CssBaseline />
+      <SignInOutContainer />
     </div>
   );
 }
