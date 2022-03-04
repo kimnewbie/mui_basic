852d907492fa55d636fe6748707873fcb3bf0527
youjininreallife <gracecancho@gmail.com>
2022-03-04 11:03:09 +0900

    btn에 다른 style 적용해보기

--- src/materialUIReactTutorial/components/TestPage.js
+++ src/materialUIReactTutorial/components/TestPage.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
-import { Button, ButtonGroup, Checkbox, FormControlLabel } from '@mui/material';
-import { Delete, Favorite, FavoriteBorder, Save } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField } from '@mui/material';
+import { Delete, Favorite, FavoriteBorder, Gradient, Save } from '@mui/icons-material';
 import { red } from '@mui/material/colors';
+import { display } from '@mui/system';
+
+const useStyles = makeStyles({
+    root: {
+        padding: '100px 100px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 10,
+        alignItems: 'center',
+    },
+    btn_group: {
+        display: 'flex',
+        gap: 5,
+        boxShadow: 'none !important',
+    },
+    btn: {
+        background: 'linear-gradient(45deg, #333, #1976d2)',
+        border: 0,
+        borderRadius: 15,
+        color: 'white !important',
+        padding: '0 30px',
+    },
+});
+
+const ButtonStyled = () => {
+    const classes = useStyles();
+    return <Button className={classes.btn}>Test Styled Button</Button>
+}
 
 const CheckboxExample = () => {
     const [checked, setChecked] = useState();
@@ -26,28 +55,47 @@ const CheckboxExample = () => {
 }
 
 const TestPage = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <CheckboxExample />
-            <ButtonGroup
-                variant='contained'
-                size='large'
-            >
-                <Button
-                    // disabled
-                    // endIcon={<MusicNoteOutlined />}
-                    startIcon={<Save />}
-                    color='primary'
-                >
-                    Save
-                </Button>
-                <Button
-                    startIcon={<Delete />}
+        <div className={classes.root}>
+            <>
+                <ButtonStyled />
+            </>
+            <>
+                <CheckboxExample />
+            </>
+            <>
+                <TextField
+                    variant='outlined' //'filled' | 'outlined' | 'standard'
                     color='secondary'
+                    type='email' // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Form_%3Cinput%3E_types
+                    label='email' // placeholder에 있다가 포커스되면 상단으로 이동
+                    // value='youjinkim@test.com'
+                    placeholder='youjinkim@test.com' // 라벨이 위로 올라가면 보임
+                />
+            </>
+            <>
+                <ButtonGroup
+                    variant='contained'
+                    size='large'
+                    className={classes.btn_group}
                 >
-                    Discard
-                </Button>
-            </ButtonGroup>
+                    <Button
+                        // disabled
+                        // endIcon={<MusicNoteOutlined />}
+                        startIcon={<Save />}
+                        color='primary'
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        startIcon={<Delete />}
+                        color='secondary'
+                    >
+                        Discard
+                    </Button>
+                </ButtonGroup>
+            </>
         </div>
     );
 };
